fb394f97fadc9e57bec57b697343de23cddd1dda
JesusDrm <jesusdiego401rm@gmail.com>
2024-01-11 12:16:51 -0600

    Eliminar archivos del repositorio

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php    
-    session_destroy();
-    $parametros_cookies = session_get_cookie_params(); 
-    setcookie(session_name(),0,1,$parametros_cookies["path"]);
-    header('Location: login.php');
-?>
